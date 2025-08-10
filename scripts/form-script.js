e2a3aa1a32f24d143c33d61d42461495f94c58d8
mungunzaya-e <endy.mun0315@gmail.com>
2025-08-11 01:29:00 +0800

    W05 assignment - Product review form

---
+++ scripts/form-script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYearElement = document.querySelector("#currentyear");
+    const lastModifiedElement = document.querySelector("#lastModified");
+
+    const date = new Date();
+    currentYearElement.textContent = date.getFullYear();
+
+    if (document.lastModified) {
+        const modifiedDate = new Date(document.lastModified).toLocaleString();
+        lastModifiedElement.textContent = "Last Modified: " + modifiedDate;
+    }
+
+    let reviewCount = localStorage.getItem("review-count");
+    reviewCount = reviewCount ? parseInt(reviewCount) : 0;
+    reviewCount ++;
+
+    localStorage.setItem("review-count", reviewCount);
+    document.getElementById("review-count").textContent = reviewCount;
+});
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const selectProduct = document.getElementById("name-select");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    selectProduct.appendChild(option);
+})
+
+
